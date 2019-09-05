7d90bf6e424d23f12dbd765e0b47e117f1712dd2
Alexa-Brown <browam18@wfu.edu>
2019-09-05 16:45:38 -0400

    beginning to work on the problem

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
+
+/*Alexa Brown, Project 1, CSC 112, due 9/12/19*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
-
+#include <ctime>
+#include <cstdlib>
 
 int main() {
 
-    vector<int> cols;
-    int rows;
-    vector<vector<int>>matrix (rows, cols);
-   
+    int rows = 3;
+    int randomCol;
+    int randomRow;
+    int userCol;
+    int userRow;
+    vector<int> first (3, rows);
+    vector<vector<int>> matrix(3,first ); //creating the battleship field
+
+    srand(time(0));
+     randomCol = rand() % 3 + 1;
+     randomRow = rand() % 3 + 1;
+
+
+     cout << "Where do you think the location is? Enter two numbers (row and column)"<< endl;
+     cin >> userCol;
+     cin >> userRow;
+     if (userCol == randomCol){
+         if (userRow == randomRow){
+             cout << "hi";
+         }
+     }
+     cout << "hello";
 
 
     return 0;
