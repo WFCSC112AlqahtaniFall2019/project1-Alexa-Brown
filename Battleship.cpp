abf06ddf0f2682be01b7ec09214a629d9053cafb
Alexa-Brown <browam18@wfu.edu>
2019-09-10 18:43:55 -0400

    Completed new requirements

--- Battleship.cpp
+++ Battleship.cpp
@@ -22,10 +22,10 @@ int main() {
     bool guess = false; // if the user's guess is true or not
 
     vector<int> first(3, rows);
-    vector<vector<int>> matrix{ //creating the battleship field
-            {1 , 1, 1},
-            {1, 1, 1},
-            {1, 1, 1}
+    vector<vector<char>> matrix{ //creating the battleship field
+            {'?' , '?', '?'},
+            {'?' , '?', '?'},
+            {'?' , '?', '?'}
     };
 
 
@@ -52,20 +52,32 @@ cout << "This is the game board:" << endl;
     cin >> userCol; //allowing the user to guess which column
     cin >> userRow; //allowing the user to guess which row
 
+    if (userCol > 3 || userCol < 0 || userRow > 3 || userRow < 0){
+        cout << "Please start over and enter a guess within the range." << endl;
+        break;
+    }
+
+
     userCol = userCol - 1;//The matrices are in vectors from 0 to 2 so this keeps the numbers the same
     userRow = userRow - 1;
 
 
+    if (userCol != randomCol || userRow != randomRow) { //using a loop to see if the user's guess matches the randomly generated answers
+            matrix[userCol][userRow] = '@' ;
+            guess = false;
+        }
+
+
     if (userCol == randomCol) { //using a loop to see if the user's guess matches the randomly generated answers
         if (userRow == randomRow) {
-            matrix[userCol][userRow] = 2;
+            matrix[userCol][userRow] = '!';
             guess = true;
         }
     }
 
 
     cout << endl;
-    cout << "Updated play board: (2 means you've selected the correct spot!)" << endl;
+    cout << "Updated play board: (@ is where you've guessed and ! means you've selected the correct spot!)" << endl;
     for (int i = 0; i < matrix.size(); i++) { //printing the new 3x3 matrix since it was updated
         for (int j = 0; j < matrix[i].size(); j++) {
             cout << matrix[i][j];
@@ -84,6 +96,6 @@ cout << "This is the game board:" << endl;
     cout << numuserguesses << endl;
     //showing the user how many times they guessed before finishing the program
 
-    
+
     return 0;
 }
